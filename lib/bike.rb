83af92a42687883157dc82d74a4648abca491f43
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-03-20 16:57:40 +0000

    added working? test

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,9 @@
 class Bike
+  def initialize
+    @working = true
+  end
 
-def working?
-end
-  
+  def working?
+    @working
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,20 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it {should respond_to (:release_bike)}
+end
+
+describe DockingStation do
+  it "releases a bike" do
+    # station = DockingStation.new
+     expect(DockingStation.new.release_bike.class).to eq(Bike)
+  end
+end
+
+
+describe DockingStation do
+  it 'expects bike to be working' do
+  expect(DockingStation.new.release_bike.working?).to be true
+  end
 end
